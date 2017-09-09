d7459ca33229964a584905877226fdf7de7755f2
stolarz <stolarz92@gmail.com>
2017-09-09 12:52:30 +0200

    Add bootstrap v4 alpha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { Angular2TokenService } from 'angular2-token';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginFormComponent } from './login-form/login-form.component';
@@ -25,7 +25,8 @@ import { RegisterFormComponent } from './register-form/register-form.component';
     BrowserModule,
     AppRoutingModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    NgbModule.forRoot(),
   ],
   providers: [Angular2TokenService],
   bootstrap: [AppComponent]

fc6aae3f796541a6ac00a173e71ab3a6fb0c3c89
stolarz <stolarz92@gmail.com>
2017-09-17 12:46:20 +0200

    Refactor components

--- src/app/todos/todo-list/todo-item/todo-item.component.ts
+++ src/app/todos/todo-list/todo-item/todo-item.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from '../../todo.model';
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.sass']
 })
 export class TodoItemComponent implements OnInit {
-
+  @Input() todo: Todo;
   constructor() { }
 
   ngOnInit() {

303c97e9b88bf7d7aefafaee1f856ba79cff95e1
stolarz <stolarz92@gmail.com>
2017-09-17 20:10:49 +0200

    Add form initialization

--- src/app/todos/todo-edit/todo-edit.component.ts
+++ src/app/todos/todo-edit/todo-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {FormControl, FormGroup} from '@angular/forms';
+import {TodoService} from "../todo.service";
 
 @Component({
   selector: 'app-todo-edit',
@@ -9,8 +11,12 @@ import {ActivatedRoute, Params} from '@angular/router';
 export class TodoEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private todoService: TodoService,
+  ) { }
 
   ngOnInit() {
     this.route.params
@@ -18,8 +24,22 @@ export class TodoEditComponent implements OnInit {
         (params: Params) => {
           this.id = +params['id'];
           this.editMode = params['id'] != null;
+          this.initForm();
         }
       )
+  }
+
+  private initForm() {
+    let todoTitle = '';
+
+    if (this.editMode) {
+      const todo = this.todoService.getTodo(this.id);
+        todoTitle = todo.title;
+    }
+
+    this.recipeForm = new FormGroup({
+      'title': new FormControl(),
+    });
   }
 
 }

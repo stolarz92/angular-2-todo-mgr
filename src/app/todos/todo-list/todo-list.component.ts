43e312c5926646c53852f845e37b7eaca52d3752
stolarz <stolarz92@gmail.com>
2017-09-17 11:53:53 +0200

    Add static todo

--- src/app/todos/todo-list/todo-list.component.ts
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Todo } from '../todo.model';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.sass']
 })
 export class TodoListComponent implements OnInit {
+  todos: Todo[] = [
+    new Todo('Test Title'),
+  ];
 
   constructor() { }
 
